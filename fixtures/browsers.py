1cd4b11feb807cf8d65e5a9e9aa1548236028c63
UseYourFist <spr47@mail.ru>
2026-01-08 12:03:14 +0700

    Параметризация Pytest. Практика работы с pytest параметризацией

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import Playwright, Page
+from playwright.sync_api import Playwright, Page, sync_playwright
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright: Playwright):
@@ -30,3 +30,10 @@ def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -
     page = context.new_page()
     yield page
     browser.close()
+
+@pytest.fixture
+def chromium_page() -> Page:
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        yield browser.new_page()
+        browser.close()
